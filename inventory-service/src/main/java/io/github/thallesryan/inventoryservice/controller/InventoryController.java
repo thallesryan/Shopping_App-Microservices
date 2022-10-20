bb6a521b0fd477f1c1fb8391047c7825107ddefb
ThallesRyan <thallesryan2@gmail.com>
2022-10-20 13:18:09 -0300

    Impl inventory-service

---
+++ inventory-service/src/main/java/io/github/thallesryan/inventoryservice/controller/InventoryController.java
@@ -0,0 +1,25 @@
+package io.github.thallesryan.inventoryservice.controller;
+import io.github.thallesryan.inventoryservice.dto.InventoryResponse;
+import io.github.thallesryan.inventoryservice.service.InventoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/inventory")
+@RequiredArgsConstructor
+public class InventoryController {
+
+    private final InventoryService inventoryService;
+
+    // http://localhost:8082/api/inventory/iphone-13,iphone13-red
+
+    // http://localhost:8082/api/inventory?skuCode=iphone-13&skuCode=iphone13-red
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode) {
+        return inventoryService.isInStock(skuCode);
+    }
+}
---
+++ inventory-service/src/main/java/io/github/thallesryan/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,10 @@
+package io.github.thallesryan.inventoryservice.repository;
+
+import io.github.thallesryan.inventoryservice.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    List<Inventory> findBySkuCodeIn(List<String> skuCode);
+}
